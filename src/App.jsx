f9c8105f2f8dd3ce90240971f88481a30aec38cc
DTSdell15 <conexiongraf@gmail.com>
2024-12-06 13:31:25 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ResponsivaGenerator from './components/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsivaGenerator />
+    </div>
+  );
+}
+
+export default App;
